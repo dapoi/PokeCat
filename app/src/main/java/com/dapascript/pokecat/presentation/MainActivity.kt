42484904acaccde510015984d961f97286b7d4e4
Luthfi Daffa Prabowo <daffaprabowo5@gmail.com>
2023-09-03 13:34:57 +0700

    add splashscreen & change icon launcher

--- app/src/main/java/com/dapascript/pokecat/presentation/MainActivity.kt
+++ app/src/main/java/com/dapascript/pokecat/presentation/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.dapascript.pokecat.presentation
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.dapascript.pokecat.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val splashScreenViewModel: SplashScreenViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().setKeepOnScreenCondition { splashScreenViewModel.isLoading.value }
         setContentView(R.layout.activity_main)
     }
 }
